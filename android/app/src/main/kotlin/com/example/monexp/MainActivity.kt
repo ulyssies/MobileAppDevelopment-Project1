fc9e2dfa0765cdf02af6da04b3229bd6e9fc3917
anhltr <anhltrann@gmail.com>
2024-07-01 22:14:14 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/monexp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.monexp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/all_expenses.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../widgets/all_expenses_screen/all_expenses_fetcher.dart';
+
+class AllExpenses extends StatefulWidget {
+  const AllExpenses({super.key});
+  static const name = '/all_expenses';
+
+  @override
+  State<AllExpenses> createState() => _AllExpensesState();
+}
+
+class _AllExpensesState extends State<AllExpenses> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Expenses',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: const AllExpensesFetcher(),
+    );
+  }
+}
